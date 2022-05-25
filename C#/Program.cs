28812e2c1d3323f5141deb81eab19536573bfc9a
juanlavallen <juanlavallen02@gmail.com>
2022-05-25 00:37:57 -0300

    Defining variables of type int

--- C#/Program.cs
+++ C#/Program.cs
@@ -4,3 +4,8 @@
 string country = "Argentina";
 string region  = "LATAM";
 Console.WriteLine("Region: " + region + " - " + "Country: " + country);
+
+// Defining variables of type int
+int quantity = 10;
+int price    = 20;
+Console.WriteLine("Quantity: " + quantity + " - " + "Price: " + price);
