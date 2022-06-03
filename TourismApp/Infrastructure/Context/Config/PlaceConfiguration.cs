76f4d77e8bc099a39c9cba5f997a88e083baa706
juanlavallen <juanlavallen02@gmail.com>
2022-06-03 14:39:45 -0300

    Config file

---
+++ TourismApp/Infrastructure/Context/Config/PlaceConfiguration.cs
@@ -0,0 +1,21 @@
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Context.Config {
+    public class PlaceConfiguration : IEntityTypeConfiguration<Place> {
+
+        public void Configure(EntityTypeBuilder<Place> builder) {
+            builder.Property(place => place.Name).HasMaxLength(50);
+            builder.Property(place => place.Description).IsRequired();
+            builder.Property(place => place.EstimatedCosts).IsRequired();
+
+            /*RelationShips*/
+            builder.HasOne(category => category.Category).WithMany()
+                .HasForeignKey(place => place.CategoryId);
+
+            builder.HasOne(country => country.Country).WithMany()
+                .HasForeignKey(place => place.CountryId);
+        }
+    }
+}
