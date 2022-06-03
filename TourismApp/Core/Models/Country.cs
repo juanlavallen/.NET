04a40950e49b0f929fabfffa7d90a3f4d732bf6a
juanlavallen <juanlavallen02@gmail.com>
2022-06-03 00:28:43 -0300

    Category & Country model created

---
+++ TourismApp/Core/Models/Country.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models {
+    public class Country {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
