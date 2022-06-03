a11090d05b4a6671f39000fe8c763c76fea156c7
juanlavallen <juanlavallen02@gmail.com>
2022-06-03 14:41:36 -0300

    DbSet -> Category & Country

--- TourismApp/Infrastructure/Context/ContextDb.cs
+++ TourismApp/Infrastructure/Context/ContextDb.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,13 @@ namespace Infrastructure.Context {
     public class ContextDb : DbContext {
         public ContextDb(DbContextOptions options) : base(options) {}
 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
         public DbSet<Place> Places { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        } 
     }
 }
