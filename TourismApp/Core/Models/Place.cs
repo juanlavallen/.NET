5c8de45868a1d94a244d366f4d3bf2438446b55a
juanlavallen <juanlavallen02@gmail.com>
2022-06-03 00:29:56 -0300

    ForeingKey --> [CountryId & CategoryId]

--- TourismApp/Core/Models/Place.cs
+++ TourismApp/Core/Models/Place.cs
@@ -1,10 +1,26 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Core.Models {
     public class Place {
         [Key]
         public int Id { get; set; }
-        [Required]
+
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double EstimatedCosts { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+        public int CountryId { get; set; }       
+        [ForeignKey("CountryId")]
+        public Country Country { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")] 
+        public Category Category { get; set; } 
     }
 }
