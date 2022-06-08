d3f6fbd71f7dc3d1dfd8002b546080c03c8dbb55
juanlavallen <juanlavallen02@gmail.com>
2022-06-07 21:46:38 -0300

    DatabaseSeed.sc created

---
+++ TourismApp/Infrastructure/Context/DatabaseSeed.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Context {
+    public class DatabaseSeed {
+
+        public static async Task SeedAsync(ContextDb context, ILoggerFactory loggerFactory) {
+            try {
+            }
+            catch (System.Exception) {
+                throw;
+            }
+        }
+    }
+}
