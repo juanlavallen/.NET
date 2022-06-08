268c04553882a8dde6fbd250ba6cc953c5e65fb7
juanlavallen <juanlavallen02@gmail.com>
2022-06-07 22:03:51 -0300

    Functionality to add data to the database

--- TourismApp/Infrastructure/Context/DatabaseSeed.cs
+++ TourismApp/Infrastructure/Context/DatabaseSeed.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Core.Models;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Context {
@@ -5,9 +7,42 @@ namespace Infrastructure.Context {
 
         public static async Task SeedAsync(ContextDb context, ILoggerFactory loggerFactory) {
             try {
+                if (!context.Countries.Any()) {
+                    var countriesData = File.ReadAllText("../Infrastructure/Context/SeedData/countries.json");
+                    var countries = JsonSerializer.Deserialize<List<Country>>(countriesData);
+
+                    foreach (var item in countries) {
+                        await context.Countries.AddAsync(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Categories.Any()) {
+                    var caegoriesData = File.ReadAllText("../Infrastructure/Context/SeedData/categories.json");
+                    var categories = JsonSerializer.Deserialize<List<Category>>(caegoriesData);
+
+                    foreach (var item in categories) {
+                        await context.Categories.AddAsync(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Places.Any()) {
+                    var placesData = File.ReadAllText("../Infrastructure/Context/SeedData/places.json");
+                    var places = JsonSerializer.Deserialize<List<Place>>(placesData);
+
+                    foreach (var item in places) {
+                        await context.Places.AddAsync(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
             }
-            catch (System.Exception) {
-                throw;
+            catch (System.Exception exception) {
+                var logger = loggerFactory.CreateLogger<DatabaseSeed>();
+                logger.LogError(exception.Message);
             }
         }
     }
