31c5fb5dc676e652165c411f4e03350f20d93c4c
juanlavallen <juanlavallen02@gmail.com>
2022-06-11 00:13:05 -0300

    Implementing basic methods

--- TourismApp/Infrastructure/Context/Repository.cs
+++ TourismApp/Infrastructure/Context/Repository.cs
@@ -1,14 +1,19 @@
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Context {
     public class Repository<T> : IRepository<T> where T : class {
         
-        public Task<IReadOnlyList<T>> getAll() {
-            throw new NotImplementedException();
+        private readonly ContextDb _db;
+
+        public Repository(ContextDb db) => this._db = db;
+
+        public async Task<IReadOnlyList<T>> getAll() {
+            return await _db.Set<T>().ToListAsync();
         }
 
-        public Task<T> getById(int id) {
-            throw new NotImplementedException();
+        public async Task<T> getById(int id) {
+            return await _db.Set<T>().FindAsync(id);
         }
     }
 }
