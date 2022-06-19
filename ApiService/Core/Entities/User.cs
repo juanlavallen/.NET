d8e647a144a05ce6cb59ed8b8e00ad6c260d3afb
juanlavallen <juanlavallen02@gmail.com>
2022-06-19 11:45:36 -0300

    Entity User added

--- ApiService/Core/Entities/User.cs
+++ ApiService/Core/Entities/User.cs
@@ -2,16 +2,5 @@ namespace Core.Entities
 {
     public class User
     {
-        [Key]
-        public int Id { get; set; }
-        
-        [Required]
-        public string Name { get; set; }
-        
-        [Required]
-        public string Email { get; set; }
-        
-        [Required]
-        public string Password { get; set; }
     }
 }
