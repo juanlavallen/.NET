c39ffba452acd993b363b692b8b779f792672896
juanlavallen <juanlavallen02@gmail.com>
2022-06-22 19:17:40 -0300

    DbSet Users added

--- ApiService/Infrastructure/Context/ContextDb.cs
+++ ApiService/Infrastructure/Context/ContextDb.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Core.Entities;
 
 namespace Infrastructure.Context
 {
     public class ContextDb : DbContext
     {
         public ContextDb(DbContextOptions options) : base(options) { }
+
+        DbSet<User> Users { get; set; }
     }
 }
