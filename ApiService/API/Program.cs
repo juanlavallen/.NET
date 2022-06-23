7a1c3dd93d756c697a244e8b2803d5b5c4a6ba59
juanlavallen <juanlavallen02@gmail.com>
2022-06-23 20:03:37 -0300

    Service DbContext added

--- ApiService/API/Program.cs
+++ ApiService/API/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+var dbConnect = builder.Configuration.GetConnectionString("ConnectionDB");
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ContextDb>(options => options.UseMySql(dbConnect, ServerVersion.AutoDetect(dbConnect)));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
