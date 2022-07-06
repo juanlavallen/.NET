67dbc5c22eb52523be19eef34f7c117477f78d2e
juanlavallen <juanlavallen02@gmail.com>
2022-07-05 23:20:10 -0300

    StringBasicLinq method added

--- LinqSnippets/Snippets.cs
+++ LinqSnippets/Snippets.cs
@@ -7,6 +7,23 @@ namespace LinqSnippets
 {
     public class Snippets
     {
-        
+        static public void StringBasicLinq()
+        {
+            string[] cars = { "Raptor", "Ferrari", "BMW" };
+
+            // SELECT * of Cars (SELECT ALL Cars)
+            var allCars = from car in cars select car;
+            foreach (var car in allCars)
+            {
+                Console.WriteLine(car);
+            }
+
+            // SELECT WHERE car is BWM (SELECT BMWs)
+            var bmwList = from car in cars where car.Contains("BMW") select car;
+            foreach (var bmw in bmwList)
+            {
+                Console.WriteLine(bmw);
+            }
+        }
     }
 }
