3ab5f265e6f91533af1455c1e72c3103962d5899
juanlavallen <juanlavallen02@gmail.com>
2022-07-05 23:15:14 -0300

    Enterprise entity added

---
+++ LinqSnippets/Enterprise.cs
@@ -0,0 +1,9 @@
+namespace LinqSnippets
+{
+    public class Enterprise
+    {
+        public int        Id        { get; set; }
+        public string     Name      { get; set; }
+        public Employee[] Employees { get; set; }
+    }
+}
