4d1da3d39d4ffaa90d4ba511327b13d7077afb6e
juanlavallen <juanlavallen02@gmail.com>
2022-07-14 20:24:00 -0300

    User model added

---
+++ University/Core/Models/User.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        public string FirstName { get; set; }
+     
+        [Required]
+        public string LastName  { get; set; }
+     
+        [Required, EmailAddress]
+        public string Email     { get; set; }
+     
+        [Required]
+        public string Password  { get; set; }
+    }
+}
