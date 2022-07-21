1e03a3458a6ec9accb0fe95389956b9f7ba600c8
juanlavallen <juanlavallen02@gmail.com>
2022-07-20 23:32:07 -0300

    CommandReadDto created

---
+++ MinimalAPI/Dtos/CommandReadDto.cs
@@ -0,0 +1,13 @@
+namespace MinimalAPI.Dtos
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+
+        public string HowTo { get; set; }
+
+        public string Plataform { get; set; }
+
+        public string CommandLine { get; set; }
+    }
+}
